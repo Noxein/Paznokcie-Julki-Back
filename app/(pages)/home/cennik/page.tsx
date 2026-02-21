a77748e6d2b45ce8f0837ff46edea3369d71581b
Noxein <138109221+Noxein@users.noreply.github.com>
2026-02-21 09:46:54 +0100

    changed static to dynamic page

--- app/(pages)/home/cennik/page.tsx
+++ app/(pages)/home/cennik/page.tsx
@@ -1,9 +1,11 @@
 import { getPrices } from "@/actions";
 import Cennik from "@/app/components/Cennik";
+import { cookies } from "next/headers";
 
 async function page() {
 
     const prices = await getPrices()
+    cookies()
     return ( 
         <Cennik prices={prices}/>
      );
